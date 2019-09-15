a0bdd9a239926b21a6246c4a0a8221c5fd66ceb0
vishalpatil0 <vishalpgatil10@gmail.com>
2019-09-15 18:10:48 +0530

    project

---
+++ JS Project/prototype1.js
@@ -0,0 +1,20 @@
+<html>
+<head> welcome to unit convertor</head>
+<script languagage="javascript" type="text/javascript">
+function length()
+{
+		with(document.forms.convert)
+		{
+			a=parseInt(n1.value)
+			c=a/100
+			v_abc.value=c
+		}
+		script>
+	</head>
+	<body>
+		<form method="post" name="convert"><p><center>
+					form onclick addition3 without getId and answer in text box with 'with keyword'<br><br>
+			centimeters  <center><input type="text" name="n1"/>   <br></center>
+			meters        <center><input type="text" name="v_abc"/><br></center>
+			<input type="button" name="convert" value="convert" onClick="length()"/><br>	
+		</form>
